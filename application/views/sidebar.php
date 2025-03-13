8decc2bd5fdba06c71766b7a8fb6ac53a4b60b21
NandaRamdania21 <nanda.ramdania.fict@krw.horizon.ac.id>
2025-03-13 20:05:14 +0700

    ada calender

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -2,15 +2,10 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
-<<<<<<< HEAD
                 <div class="sidebar-brand-icon rotate-n-15">
                  
                 </div>
                 <img src="assets/img/Siix.jpg" alt="Logo" width="180" height="60" class="mr-2">
-=======
-                
-                <img src="assets/img/Siix.jpg" alt="Logo" width="170" height="60" class="mr-2">
->>>>>>> fa442be (sudah bisa login)
                 
             </a>
 
@@ -102,23 +97,4 @@
                     <i class="fas fa-fw fa-user-shield"></i>
                     <span>USER MANAGEMENT</span></a>
             </li>
-
-<<<<<<< HEAD
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            <!-- Sidebar Message -->
-            <div class="sidebar-card d-none d-lg-flex">
-                <img class="sidebar-card-illustration mb-2" src="img/undraw_rocket.svg" alt="...">
-                <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components, and more!</p>
-                <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to Pro!</a>
-            </div>
-
-=======
->>>>>>> fa442be (sudah bisa login)
         </ul>
